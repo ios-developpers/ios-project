65abd2ae8734d030a9ebc2b05f3499d20f32cdce
Wawamaniac <kevin.langles@gmail.com>
2014-02-13 11:53:29 +0100

    inital commit of empty ios project

---
+++ Conference/Conference/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Conference
+//
+//  Created by Kevin Langles on 13/02/2014.
+//  Copyright (c) 2014 ios-developpers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Conference/Conference/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Conference
+//
+//  Created by Kevin Langles on 13/02/2014.
+//  Copyright (c) 2014 ios-developpers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
