a2df084f1d707e384e8ee6adb0e3ab56eec4b433
Wawamaniac <kevin.langles@gmail.com>
2014-03-04 12:57:47 +0100

    interface listener commit

---
+++ Conference/Conference/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  Conference
+//
+//  Created by ig_2014 on 04/03/2014.
+//  Copyright (c) 2014 ios-developpers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
---
+++ Conference/Conference/MyViewController.m
@@ -0,0 +1,45 @@
+//
+//  MyViewController.m
+//  Conference
+//
+//  Created by ig_2014 on 04/03/2014.
+//  Copyright (c) 2014 ios-developpers. All rights reserved.
+//
+
+#import "MyViewController.h"
+
+@interface MyViewController ()
+- (IBAction)addSalonAction:(UIBarButtonItem *)sender;
+
+@end
+
+@implementation MyViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)addSalonAction:(UIBarButtonItem *)sender
+{
+    NSLog(@"%@", @"coucou keke");
+}
+@end
