55aff95dd622844a777e472ba6426db4af8bfa48
Wawamaniac <kevin.langles@gmail.com>
2014-03-05 10:22:07 +0100

    first commit

---
+++ Conference/Conference/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  Conference
+//
+//  Created by ig_2014 on 05/03/2014.
+//  Copyright (c) 2014 ios-developpers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Document : NSObject
+
+@end
---
+++ Conference/Conference/Document.m
@@ -0,0 +1,13 @@
+//
+//  Document.m
+//  Conference
+//
+//  Created by ig_2014 on 05/03/2014.
+//  Copyright (c) 2014 ios-developpers. All rights reserved.
+//
+
+#import "Document.h"
+
+@implementation Document
+
+@end
