45b6d9ac882bfa68c38e8da644b14b8cd1836137
Christophe <christophe.pont@icloud.com>
2014-04-07 18:43:13 +0200

    Persistance document
    + petite correction sur liste salon (affichage moisi)

--- Conference/Conference/Document.h
+++ Conference/Conference/Document.h
@@ -7,13 +7,15 @@
 #import <Foundation/Foundation.h>
 
 //INTERFACE
-@interface Document : NSObject
+@interface Document : NSObject <NSCoding>
 
 //PROPERTIES
 @property(nonatomic,copy)NSString* name;
 @property(nonatomic,copy)NSString* url;
 
 //METHODS
+-(id)initWithCoder:(NSCoder *)aDecoder;
 -(id) initWithName:(NSString *)_name andUrl:(NSString *)_url;
 
+-(void)encodeWithCoder:(NSCoder *)aCoder;
 @end
--- Conference/Conference/Document.m
+++ Conference/Conference/Document.m
@@ -14,8 +14,19 @@
 //SYNTHESIZE
 @synthesize name;
 @synthesize url;
+static NSString *const kName = @"Name";
+static NSString *const kUrl = @"Url";
 
 //METHODS
+-(id)initWithCoder:(NSCoder *)aDecoder
+{
+    if ((self=[super init])) {
+        NSString *pName = [aDecoder decodeObjectForKey:kName];
+        NSString *pUrl = [aDecoder decodeObjectForKey:kUrl];
+        return [self initWithName:pName andUrl:pUrl];
+    }
+    return self;
+}
 
 -(id) initWithName:(NSString*)_name andUrl:(NSString*)_url
 {
@@ -32,5 +43,10 @@
     return self;
 }
 
+-(void)encodeWithCoder:(NSCoder *)aCoder
+{
+    [aCoder encodeObject:name forKey:kName];
+    [aCoder encodeObject:url forKey:kUrl];
+}
 
 @end
